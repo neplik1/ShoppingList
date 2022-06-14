896ddc724e3929617cc32ececcd803fd61a9d7f9
neplik1 <neplik1@gmail.com>
2022-06-14 18:37:33 +0300

    created fragment and add in Activity

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -15,7 +15,6 @@ import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
     private lateinit var viewModel: ShopItemViewModel
-    private lateinit var shopListAdapter: ShopListAdapter
 
     private lateinit var tilName: TextInputLayout
     private lateinit var tilCount: TextInputLayout
@@ -135,27 +134,26 @@ class ShopItemActivity : AppCompatActivity() {
         addSaveButton = findViewById(R.id.add_save_button)
     }
 
-    companion object {
-        private const val EXTRA_SCREEN_MODE = "extra_mode"
-        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
-        private const val MODE_EDIT = "mode_edit"
-        private const val MODE_ADD = "mode_add"
-        private const val MODE_UNKNOW = ""
+        companion object {
+            private const val EXTRA_SCREEN_MODE = "extra_mode"
+            private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+            private const val MODE_EDIT = "mode_edit"
+            private const val MODE_ADD = "mode_add"
+            private const val MODE_UNKNOW = ""
 
 
-        fun newIntentAddItem(context: Context): Intent {
-            val intent = Intent(context, ShopItemActivity::class.java)
-            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
-            return intent
+            fun newIntentAddItem(context: Context): Intent {
+                val intent = Intent(context, ShopItemActivity::class.java)
+                intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+                return intent
+            }
+
+            fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+                val intent = Intent(context, ShopItemActivity::class.java)
+                intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+                intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+                return intent
+            }
         }
 
-        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
-            val intent = Intent(context, ShopItemActivity::class.java)
-            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
-            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
-            return intent
-        }
     }
-
-
-}
